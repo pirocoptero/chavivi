3de6fe872089783914fafeb939fa0574b67d884c
Adriano Ferreira <piro@outlook.com.br>
2022-01-20 16:42:09 -0300

    Ajustes de layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;600;800&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;600;800&display=swap" rel="stylesheet" />                    
                 </Head>
                 <body>
                     <Main />
